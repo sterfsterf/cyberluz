3fa9d39584ea172b09a44146289216252c071062
sterf <katesterf@gmail.com>
2024-01-18 13:38:39 -0500

    added modes and moved pins around to support the uno r3

--- testfade/testfade.ino
+++ testfade/testfade.ino
@@ -1,7 +1,10 @@
 //LED Pins
-int led_uv = 10;
-int led_oj = 11;
-int led_pk = 12;
+int led_uv_left = 11;
+int led_oj_left = 10;
+int led_pk_left = 9;
+int led_uv_right = 6;
+int led_oj_right = 5;
+int led_pk_right = 3;
 
 int value, value2, value3;
 long time=0;
@@ -11,7 +14,7 @@ int displace = 1300; //oj
 int displace2 =2600; //pk
 
 //Analog Button 
-int btn = 3;
+int btn = 2;
 
 //button state variables
 int butState = 0; //Last Button State
@@ -26,9 +29,12 @@ int modeState = 0; //Last Mode State
 //===============================================================================
 void setup() {
   pinMode(btn, INPUT);
-  pinMode(led_uv, OUTPUT);
-  pinMode(led_oj, OUTPUT);
-  pinMode(led_pk, OUTPUT);
+  pinMode(led_uv_left, OUTPUT);
+  pinMode(led_oj_left, OUTPUT);
+  pinMode(led_pk_left, OUTPUT);
+  pinMode(led_uv_right, OUTPUT);
+  pinMode(led_oj_right, OUTPUT);
+  pinMode(led_pk_right, OUTPUT);
   //butState = digitalRead(btn);
   Serial.begin(9600);
 }
@@ -53,40 +59,76 @@ void loop() {
     switch (mode) {
       case 2:            //Start with Case 2 because Case 1 is considered the default 
 //      Solid ("uv")  
-      analogWrite(led_uv, 255);
-      analogWrite(led_pk, 0);
-      analogWrite(led_oj, 0);
+      analogWrite(led_uv_left, 255);
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_oj_left, 0);
+      analogWrite(led_uv_right, 255);
+      analogWrite(led_pk_right, 0);
+      analogWrite(led_oj_right, 0);
       break;
       case 3:
 //      Solid ("oj")
-      analogWrite(led_oj, 255);
-      analogWrite(led_uv, 0);
-      analogWrite(led_pk, 0);
+      analogWrite(led_oj_left, 255);
+      analogWrite(led_uv_left, 0);
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_oj_right, 255);
+      analogWrite(led_uv_right, 0);
+      analogWrite(led_pk_right, 0);
       break;
       case 4:
 //      Solid ("pk")
-      analogWrite(led_pk, 255);
-      analogWrite(led_uv, 0);
-      analogWrite(led_oj, 0);
+      analogWrite(led_pk_left, 255);
+      analogWrite(led_uv_left, 0);
+      analogWrite(led_oj_left, 0);
+      analogWrite(led_pk_right, 255);
+      analogWrite(led_uv_right, 0);
+      analogWrite(led_oj_right, 0);
       break;
       case 5:
  //     Breathe ("uv")   
  // put breath code here scrub
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_oj_left, 0); 
+      analogWrite(led_pk_right, 0);
+      analogWrite(led_oj_right, 0); 
       time = millis();
       value = 128+127*cos(2*PI/periode*time);
-      analogWrite(led_uv, value);           // sets the value (range from 0 to 255) 
+      analogWrite(led_uv_left, value);           // sets the value (range from 0 to 255) 
+      analogWrite(led_uv_right, value); 
       break;
       case 6:
- //     Breathe ("oj")   
- // put breath code here scrub
+ //     Breathe ("oj")  
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_uv_left, 0); 
+      analogWrite(led_pk_right, 0);
+      analogWrite(led_uv_right, 0); 
+       time = millis();
+      value = 128+127*cos(2*PI/periode*time);
+      analogWrite(led_oj_left, value);   
+      analogWrite(led_oj_right, value);  
       break;
       case 7:
- //     Breathe ("pk")   
- // put breath code here scrub
+ //     Breathe ("pk") 
+      analogWrite(led_uv_left, 0);
+      analogWrite(led_oj_left, 0);  
+      analogWrite(led_uv_right, 0);
+      analogWrite(led_oj_right, 0);  
+       time = millis();
+      value = 128+127*cos(2*PI/periode*time);
+      analogWrite(led_pk_left, value);   
+      analogWrite(led_pk_right, value); 
       break;
      case 8:
  //     Breathe ("complex")   
- // put complex code here scrub
+ // put something cool here scrub
+      time = millis();
+      value = 128+127*cos(2*PI/periode*time);
+      analogWrite(led_uv_left, value);
+      analogWrite(led_oj_left, value); 
+      analogWrite(led_pk_left, value);     
+      analogWrite(led_uv_right, value);
+      analogWrite(led_oj_right, value); 
+      analogWrite(led_pk_right, value);  
       break;
      case 9:
  //     Breathe ("complex chase")   
@@ -95,9 +137,21 @@ void loop() {
       value = 128+127*cos(2*PI/periode*time);
       value2 = 128+127*cos(2*PI/periode*(displace-time));
       value3 = 128+127*cos(2*PI/periode*(displace2-time));
-      analogWrite(led_uv, value);
-      analogWrite(led_oj, value2); 
-      analogWrite(led_pk, value3);     
+      analogWrite(led_uv_left, value);
+      analogWrite(led_oj_left, value2); 
+      analogWrite(led_pk_left, value3);     
+      analogWrite(led_uv_right, value);
+      analogWrite(led_oj_right, value2); 
+      analogWrite(led_pk_right, value3);   
+      break;
+     case 10:
+ //     solid all   
+      analogWrite(led_uv_left, value);
+      analogWrite(led_oj_left, value2); 
+      analogWrite(led_pk_left, value3);     
+      analogWrite(led_uv_right, value);
+      analogWrite(led_oj_right, value2); 
+      analogWrite(led_pk_right, value3);   
       break;
       default:
       mode = 1;
@@ -109,54 +163,76 @@ void loop() {
     switch (modeState) {
       case 2:            //Start with Case 2 because Case 1 is considered the default 
 //      Solid ("uv")  
-      analogWrite(led_uv, 255);
-      analogWrite(led_pk, 0);
-      analogWrite(led_oj, 0);
+      analogWrite(led_uv_left, 255);
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_oj_left, 0);
+      analogWrite(led_uv_right, 255);
+      analogWrite(led_pk_right, 0);
+      analogWrite(led_oj_right, 0);
       break;
       case 3:
 //      Solid ("oj")
-      analogWrite(led_oj, 255);
-      analogWrite(led_uv, 0);
-      analogWrite(led_pk, 0);
+      analogWrite(led_oj_left, 255);
+      analogWrite(led_uv_left, 0);
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_oj_right, 255);
+      analogWrite(led_uv_right, 0);
+      analogWrite(led_pk_right, 0);
       break;
       case 4:
 //      Solid ("pk")
-      analogWrite(led_pk, 255);
-      analogWrite(led_uv, 0);
-      analogWrite(led_oj, 0);
+      analogWrite(led_pk_left, 255);
+      analogWrite(led_uv_left, 0);
+      analogWrite(led_oj_left, 0);
+      analogWrite(led_pk_right, 255);
+      analogWrite(led_uv_right, 0);
+      analogWrite(led_oj_right, 0);
       break;
       case 5:
  //     Breathe ("uv")   
-      analogWrite(led_pk, 0);
-      analogWrite(led_oj, 0);
+ // put breath code here scrub
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_oj_left, 0); 
+      analogWrite(led_pk_right, 0);
+      analogWrite(led_oj_right, 0); 
       time = millis();
       value = 128+127*cos(2*PI/periode*time);
-      analogWrite(led_uv, value);   
+      analogWrite(led_uv_left, value);           // sets the value (range from 0 to 255) 
+      analogWrite(led_uv_right, value); 
       break;
       case 6:
  //     Breathe ("oj")  
-      analogWrite(led_pk, 0);
-      analogWrite(led_uv, 0); 
+      analogWrite(led_pk_left, 0);
+      analogWrite(led_uv_left, 0); 
+      analogWrite(led_pk_right, 0);
+      analogWrite(led_uv_right, 0); 
        time = millis();
       value = 128+127*cos(2*PI/periode*time);
-      analogWrite(led_oj, value);   
+      analogWrite(led_oj_left, value);   
+      analogWrite(led_oj_right, value);  
       break;
       case 7:
  //     Breathe ("pk") 
-      analogWrite(led_uv, 0);
-      analogWrite(led_oj, 0);  
+      analogWrite(led_uv_left, 0);
+      analogWrite(led_oj_left, 0);  
+      analogWrite(led_uv_right, 0);
+      analogWrite(led_oj_right, 0);  
        time = millis();
       value = 128+127*cos(2*PI/periode*time);
-      analogWrite(led_pk, value);   
+      analogWrite(led_pk_left, value);   
+      analogWrite(led_pk_right, value); 
       break;
      case 8:
  //     Breathe ("complex")   
  // put something cool here scrub
       time = millis();
       value = 128+127*cos(2*PI/periode*time);
-      analogWrite(led_uv, value);
-      analogWrite(led_oj, value); 
-      analogWrite(led_pk, value);     
+      analogWrite(led_uv_left, value);
+      analogWrite(led_oj_left, value); 
+      analogWrite(led_pk_left, value);     
+      analogWrite(led_uv_right, value);
+      analogWrite(led_oj_right, value); 
+      analogWrite(led_pk_right, value);  
       break;
      case 9:
  //     Breathe ("complex chase")   
@@ -165,9 +241,21 @@ void loop() {
       value = 128+127*cos(2*PI/periode*time);
       value2 = 128+127*cos(2*PI/periode*(displace-time));
       value3 = 128+127*cos(2*PI/periode*(displace2-time));
-      analogWrite(led_uv, value);
-      analogWrite(led_oj, value2); 
-      analogWrite(led_pk, value3);    
+      analogWrite(led_uv_left, value);
+      analogWrite(led_oj_left, value2); 
+      analogWrite(led_pk_left, value3);     
+      analogWrite(led_uv_right, value);
+      analogWrite(led_oj_right, value2); 
+      analogWrite(led_pk_right, value3);   
+      break;
+     case 10:
+ //     solid all   
+      analogWrite(led_uv_left, 255);
+      analogWrite(led_oj_left, 255); 
+      analogWrite(led_pk_left, 255);     
+      analogWrite(led_uv_right, 255);
+      analogWrite(led_oj_right, 255); 
+      analogWrite(led_pk_right, 255);   
       break;
       default:
       mode = 1;
@@ -183,9 +271,10 @@ void loop() {
 //===============================================================================
 
 void ColorSelect(){
-  analogWrite(led_uv, 0);
-  analogWrite(led_pk, 0);
-  analogWrite(led_oj, 0);
+  analogWrite(led_uv_left, 0);
+  analogWrite(led_pk_left, 0);
+  analogWrite(led_oj_left, 0);
+  analogWrite(led_uv_right, 0);
+  analogWrite(led_pk_right, 0);
+  analogWrite(led_oj_right, 0);
 }
-
-
